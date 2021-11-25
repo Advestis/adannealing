7befdbbce8ddea4cc34e256cdca24654b6e4acea
pcotte <pcotte@advestis.com>
2021-11-25 18:24:56 +0100

    chg: profiler

--- code_philippe.py
+++ code_philippe.py
@@ -7,6 +7,13 @@
 # 543f88a20cf2e76f02246e49/download
 # [4] https://nathanrooy.github.io/posts/2020-05-14/simulated-annealing-with-python/
 
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+
 import adutils
 adutils.init("logger")
 from adutils import setup_logger
@@ -18,7 +25,12 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import logging
 import argparse
+import math
 import matplotlib.pyplot as plt
+from adlearn.engine import get_engine
+from time import time
+
+engine = get_engine(kind="multiproc", context="spawn", print_percent=None, max_cpus=10)
 
 from adannealing import Annealer, plot
 
@@ -121,7 +133,10 @@ def run(number_isins, do_plot, verbose=True):
     # TODO: - implement the new loss with scores, including constraints: ery important also mathemtically as
     # TODO:   I do not have anymore the quadratic term
     # TODO: - Optimise via TF
-    
+
+    logger.info("")
+    logger.info(f"Starting annealing profiler with {number_isins} isins...")
+
     selected_prices = all_prices.dropna(how="any", axis=1)
 
     chosen_isins = selected_prices.columns[:number_isins]
@@ -136,23 +151,6 @@ def run(number_isins, do_plot, verbose=True):
     # startin equi-w
     weights_day_before = pd.DataFrame(
         data=np.full(shape=(len(chosen_isins), 1), fill_value=(1.0 / number_isins)), index=[chosen_isins]
-    )
-    # just a 50% variation of 2 isins
-    weights_today = weights_day_before.copy()
-    weights_day_before.iloc[0] = weights_day_before.iloc[0] * (1 - 0.5)
-    weights_day_before.iloc[1] = weights_day_before.iloc[1] * (1 + 0.5)
-    # noinspection PyArgumentList
-    assert np.isclose(weights_day_before.sum()[0], 1)
-
-    loss_date = loss_portfolio_mean_var(
-        wt_np=weights_today.to_numpy(),
-        wt_1_np=weights_day_before.to_numpy(),
-        r_np=selected_returns.loc[date].to_numpy().reshape((number_isins, 1)),
-        risk_coeff=overall_risk_coeff,
-        eps_np=fees.to_numpy(),
-        cov_np=selected_cov.to_numpy(),
-        n=len(chosen_isins),
-        by_component=False,
     )
 
     analy_opt = analy_optim_mean_var(
@@ -195,6 +193,7 @@ def run(number_isins, do_plot, verbose=True):
     bounds = np.concatenate([bounds_min, bounds_max]).T
 
     # Using custom start temp.
+    t0 = time()
     ann = Annealer(
         loss=objective,
         weights_step_size=step_size,
@@ -207,17 +206,17 @@ def run(number_isins, do_plot, verbose=True):
         alpha=alpha,
         stopping_limit=0.001
     )
+    tf = time() - t0
     fig_hist = plot(hist, step_size=10)
     fig_hist.savefig(f"profiler/history_{number_isins}.pdf")
-    
-    error = ((analy_opt.T[0] - numerical_solution) ** 2).sum() / number_isins
 
-    logger.info(f"date : {date}. Current loss: {loss_date}")
-    logger.info(f"Numerical weights : {numerical_solution}")
+    euclidian_norm = math.sqrt(((analy_opt.T[0] - numerical_solution) ** 2).sum())
+    error = euclidian_norm / math.sqrt((analy_opt.T[0] ** 2).sum())
+
+    logger.info(f"date : {date}")
     logger.info(f"Numerical loss : {val_at_best}")
-    logger.info(f"Analytical optimum : {analy_opt.T[0]}")
     logger.info(f"Loss at analytical optimum : {loss_at_min}")
-    logger.info(f"Error : {100 * error}%")
+    logger.info(f"Error : {100 * error} %, Euclidian norm: {euclidian_norm}")
 
     if number_isins < 6 and do_plot:
         # doing a surface plot of the loss
@@ -225,7 +224,7 @@ def run(number_isins, do_plot, verbose=True):
         specs = [[None, {"rowspan": 1, "colspan": 1}] for _ in range(number_isins + 1)]
         specs[0] = [{"rowspan": number_isins + 1, "colspan": 1, "type": "surface"}, {"rowspan": 1, "colspan": 1}]
 
-        fig = make_subplots(
+        fig_ = make_subplots(
             rows=number_isins + 1,
             cols=2,
             specs=specs,
@@ -252,14 +251,14 @@ def run(number_isins, do_plot, verbose=True):
                 w_x_y = np.array([[w_x], [w_y]])
                 domain.loc[w_x, w_y] = objective_2d(w_x_y)
 
-        fig.add_trace(
+        fig_.add_trace(
             go.Surface(
                 z=domain.values, y=domain.index, x=domain.columns, colorscale="Blues", showscale=False, opacity=0.5
             ),
             row=1,
             col=1,
         )
-        fig.update_layout(
+        fig_.update_layout(
             title="Loss Portfolio Optimisation 2 Isins",
         )
 
@@ -267,7 +266,7 @@ def run(number_isins, do_plot, verbose=True):
             for i, (w_x, w_y) in enumerate(zip(x_explored, y_explored)):
                 z_explored[i] = objective_2d(np.array([[w_x], [w_y]]))
 
-        fig.add_scatter3d(
+        fig_.add_scatter3d(
             # for some reason, need to transpose
             x=y_explored,
             y=x_explored,
@@ -283,7 +282,7 @@ def run(number_isins, do_plot, verbose=True):
             row=1,
             col=1,
         )
-        fig.add_scatter3d(
+        fig_.add_scatter3d(
             # for some reason, need to transpose
             x=[numerical_solution[1]],
             y=[numerical_solution[0]],
@@ -304,7 +303,7 @@ def run(number_isins, do_plot, verbose=True):
 
             name = "w_" + str(parameter_i)
 
-            fig.add_trace(
+            fig_.add_trace(
                 go.Scatter(
                     x=np.arange(len(this_param_accepted)),
                     y=this_param_accepted,
@@ -332,7 +331,7 @@ def run(number_isins, do_plot, verbose=True):
             else:
                 ref_line = analy_opt[parameter_i][0]
 
-            fig.add_shape(
+            fig_.add_shape(
                 type="line",
                 x0=0,
                 y0=ref_line,
@@ -350,7 +349,7 @@ def run(number_isins, do_plot, verbose=True):
 
         name = "loss"
 
-        fig.add_trace(
+        fig_.add_trace(
             go.Scatter(
                 x=np.arange(len(this_param_accepted)),
                 y=this_param_accepted,
@@ -371,7 +370,7 @@ def run(number_isins, do_plot, verbose=True):
 
         ref_line = val_at_best
 
-        fig.add_shape(
+        fig_.add_shape(
             type="line",
             x0=0,
             y0=ref_line,
@@ -385,9 +384,12 @@ def run(number_isins, do_plot, verbose=True):
             ),
         )
 
-        fig.write_html(Path(path_save_images) / f"nsins_{number_isins}.html")
+        fig_.write_html(Path(path_save_images) / f"nsins_{number_isins}.html")
 
-    return error
+    logger.info("...done")
+    plt.close("all")
+
+    return error, euclidian_norm, tf
 
 
 if __name__ == "__main__":
@@ -405,21 +407,43 @@ if __name__ == "__main__":
         "-s", "--start", type=int, default=5, help="initial number of isins to use with 'profile'"
     )
     parser.add_argument(
+        "-S", "--step", type=int, default=1, help="Steps in number of isins to use with 'profile'"
+    )
+    parser.add_argument(
         "-e", "--end", type=int, default=40, help="final number of isins to use with 'profile'"
     )
     parser.add_argument(
         "-P", "--profile", action="store_true", help="Do profiling"
     )
+    parser.add_argument(
+        "-m", "--multiproc", action="store_true", help="Do profiling in parallel"
+    )
 
     args = parser.parse_args()
 
     if args.profile:
-        isins = list(range(args.start, args.end))
-        errors = [run(i, False, False) for i in isins]
-        plt.scatter(isins, errors, s=2)
-        ax = plt.gca()
-        ax.set_xlabel("# Isins")
-        ax.set_ylabel("error")
-        plt.gcf().savefig("profile.pdf")
+        if args.end != -1:
+            isins = list(range(args.start, args.end + 1, args.step))
+        else:
+            isins = list(range(args.start, len(all_prices.columns), args.step))
+        if args.multiproc:
+            errors_norms_times = engine(run, isins, do_plot=False, verbose=False)
+        else:
+            errors_norms_times = [run(i, False, False) for i in isins]
+        errors = [err[0] for err in errors_norms_times]
+        norms = [err[1] for err in errors_norms_times]
+        times = [err[2] for err in errors_norms_times]
+        fig, axes = plt.subplots(3, 1, figsize=(10, 10))
+        axes[2].set_xlabel("# Isins", fontsize=15)
+        axes[0].set_ylabel("Errors (%)", fontsize=15)
+        axes[1].set_ylabel("Euclidian norm to real", fontsize=15)
+        axes[2].set_ylabel("Annealing time (s)", fontsize=15)
+        axes[0].grid(True, ls="--", lw=0.2, alpha=0.5)
+        axes[1].grid(True, ls="--", lw=0.2, alpha=0.5)
+        axes[2].grid(True, ls="--", lw=0.2, alpha=0.5)
+        axes[0].scatter(isins, 100 * errors, s=4)
+        axes[1].scatter(isins, norms, s=4)
+        axes[2].scatter(isins, times, s=4)
+        fig.savefig(f"profiler/profile_{args.start}_{args.end}_{args.step}.pdf")
     else:
         run(args.nisins, args.plot)
